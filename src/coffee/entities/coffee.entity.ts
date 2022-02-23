7037335ecf288056c7bae5b5f326f67eb196a5c4
samLynux <samuelbisnes@gmail.com>
2022-02-23 10:47:59 +0700

    24

--- src/coffee/entities/coffee.entity.ts
+++ src/coffee/entities/coffee.entity.ts
@@ -1,8 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 
+@Entity()
 export class coffee{
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
+
+    @Column('json', {nullable: true})
     flavors: string[];
 }
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeController } from 'src/coffee/coffee.controller';
+import { coffee } from 'src/coffee/entities/coffee.entity';
 import { CoffeesService } from './coffees.service';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([coffee])
+    ],
     controllers:[
         CoffeeController
     ],
