d66cda4f2a61688b685778ff12703e0a84076917
samLynux <samuelbisnes@gmail.com>
2022-02-25 12:55:33 +0700

    55

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
 import appConfig from './config/app.config';
 import { APP_PIPE } from '@nestjs/core';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -29,7 +30,7 @@ import { APP_PIPE } from '@nestjs/core';
       load: [appConfig],
     }), 
     CoffeesModule, 
-    CoffeeRatingModule, DatabaseModule
+    CoffeeRatingModule, DatabaseModule, CommonModule
   ],
   controllers: [AppController],
   providers: [AppService,{
--- src/coffee/coffee.controller.ts
+++ src/coffee/coffee.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, SetMetadata } from '@nestjs/common';
 import { CoffeesService } from 'src/coffees/coffees.service';
 import { CreateCoffeeDto } from 'src/coffees/dto/create-coffee.dto';
 import { UpdateCoffeeDto } from 'src/coffees/dto/update-coffee.dto';
+import { Public } from 'src/common/decorators/public-decorator';
 import { PaginationQueryDTO } from 'src/common/dto/pagination-query.dto';
 
 @Controller('coffee')
@@ -10,6 +11,7 @@ export class CoffeeController {
         private readonly coffeeService: CoffeesService
         ){}
 
+    @Public()
     @Get()
     findAll (@Query() paginationQuery: PaginationQueryDTO){
         
