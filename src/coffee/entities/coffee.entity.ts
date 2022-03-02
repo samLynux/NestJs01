52bbfae535ec64572e810fb0b70200b1e1d77e76
samLynux <samuelbisnes@gmail.com>
2022-03-02 12:39:35 +0700

    76

--- src/coffee/entities/coffee.entity.ts
+++ src/coffee/entities/coffee.entity.ts
@@ -1,8 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import {Document} from 'mongoose';
 
 
-export class coffee{
-    id: number;
+@Schema()
+export class coffee extends Document {
+    @Prop()
     name: string;
+
+    @Prop()
     brand: string;
+
+    @Prop([String])
     flavors: string[];
 }
+
+export const CoffeeSchema = SchemaFactory.createForClass(coffee);
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CoffeeController } from 'src/coffee/coffee.controller';
 import { CoffeesService } from './coffees.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { coffee, CoffeeSchema } from 'src/coffee/entities/coffee.entity';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: coffee.name,
+                schema: CoffeeSchema
+            },
+        ])
+    ],
     controllers:[
         CoffeeController
     ],
