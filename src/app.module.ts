d6b8f6e5623ac1602a05bdfb70898271b36a6557
samLynux <samuelbisnes@gmail.com>
2022-03-02 12:19:43 +0700

    75

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { CoffeeController } from './coffee/coffee.controller';
 import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
